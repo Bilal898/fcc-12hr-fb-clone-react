5ad1cdb02a796208ed0bf06fec2064e14d01cdf8
Bilal989 <bfhamid@gmail.com>
2019-12-12 12:39:18 +0500

    5.588 theme fix

--- src/pages/login.js
+++ src/pages/login.js
@@ -10,32 +10,10 @@ import axios from 'axios'
 import {Link} from 'react-router-dom'
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const styles = {
-    form: {
-        textAlign: 'center'
-    },
-    image: {
-        margin: '20px auto 20px auto'
-    },
-    pageTitle: {
-        margin: '10px auto 10px auto'
-    },
-    textField: {
-        margin: '10px auto 10px auto'
-    },
-    button: {
-        marginTop: 20,
-        position: 'relative'
-    },
-    position: {
-        position: 'absolute'
-    },
-    customError: {
-        color: 'red',
-        fontSize: '0.8rem',
-        marginTop: 10
-    }
-}
+const styles = (theme) => ({
+    ...theme.spreadIt
+  });
+
 class login extends Component {
     state = {
         email: '',
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -10,35 +10,10 @@ import axios from 'axios'
 import {Link} from 'react-router-dom'
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-// const styles = (theme) => ({
-//     ...theme
-//   });
-  const styles = {
-    form: {
-        textAlign: 'center'
-    },
-    image: {
-        margin: '20px auto 20px auto'
-    },
-    pageTitle: {
-        margin: '10px auto 10px auto'
-    },
-    textField: {
-        margin: '10px auto 10px auto'
-    },
-    button: {
-        marginTop: 20,
-        position: 'relative'
-    },
-    position: {
-        position: 'absolute'
-    },
-    customError: {
-        color: 'red',
-        fontSize: '0.8rem',
-        marginTop: 10
-    }
-}
+const styles = (theme) => ({
+    ...theme.spreadIt
+  });
+
 class signup extends Component {
     state = {
         email: '',
@@ -137,7 +112,7 @@ class signup extends Component {
                         </Button>
                         <br />
                         <small>Already have an account?  
-                            <Link to="/signup"> here</Link>
+                            <Link to="/login"> here</Link>
                         </small>
                     </form>
                 </Grid>
